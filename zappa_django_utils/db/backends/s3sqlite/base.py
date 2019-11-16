bbc358ad7b8294ca11961ef6d4fe6ce222b56e94
Allen, Timothy <tallen@wharton.upenn.edu>
2019-11-15 21:06:19 -0500

    Let's try using supersqlite for a pure-Python solution.

--- zappa_django_utils/db/backends/s3sqlite/base.py
+++ zappa_django_utils/db/backends/s3sqlite/base.py
@@ -1,25 +1,28 @@
 from django.db.backends.sqlite3.base import DatabaseWrapper
 
+from hashlib import md5
 from io import BytesIO
+from logging import debug as logging_debug
+from os import path
 
 import boto3
 import botocore
-import hashlib
-import logging
-import os
+from supersqlite import sqlite3 as Database
 
 
 class DatabaseWrapper(DatabaseWrapper):
     """
-    Wraps the normal Django SQLite DB engine in an S3 backer!
-
+    Wraps the normal Django SQLite DB engine in one that shuttles the SQLite database
+    back and forth from an S3 bucket.
     """
+
+    Database = Database
 
     def load_remote_db(self):
         """
-        Load remote S3 DB
+        Load the database from the S3 storage bucket into the current AWS Lambda
+        instance.
         """
-
         signature_version = self.settings_dict.get("SIGNATURE_VERSION", "s3v4")
         s3 = boto3.resource(
             's3',
@@ -29,22 +32,28 @@ class DatabaseWrapper(DatabaseWrapper):
         if '/tmp/' not in self.settings_dict['NAME']:
             try:
                 etag = ''
-                if os.path.isfile('/tmp/' + self.settings_dict['NAME']):
-                    m = hashlib.md5()
+                if path.isfile('/tmp/' + self.settings_dict['NAME']):
+                    m = md5()
                     with open('/tmp/' + self.settings_dict['NAME'], 'rb') as f:
                         m.update(f.read())
 
-                    # In general the ETag is the md5 of the file, in some cases it's not,
-                    # and in that case we will just need to reload the file, I don't see any other way
+                    # In general the ETag is the md5 of the file, in some cases it's
+                    # not, and in that case we will just need to reload the file,
+                    # I don't see any other way
                     etag = m.hexdigest()
 
-                obj = s3.Object(self.settings_dict['BUCKET'], self.settings_dict['NAME'])
-                obj_bytes = obj.get(IfNoneMatch=etag)["Body"]  # Will throw E on 304 or 404
+                obj = s3.Object(
+                    self.settings_dict['BUCKET'],
+                    self.settings_dict['NAME'],
+                )
+                obj_bytes = obj.get(
+                    IfNoneMatch=etag,
+                )["Body"]  # Will throw E on 304 or 404
 
                 with open('/tmp/' + self.settings_dict['NAME'], 'wb') as f:
                     f.write(obj_bytes.read())
 
-                m = hashlib.md5()
+                m = md5()
                 with open('/tmp/' + self.settings_dict['NAME'], 'rb') as f:
                     m.update(f.read())
 
@@ -52,13 +61,15 @@ class DatabaseWrapper(DatabaseWrapper):
 
             except botocore.exceptions.ClientError as e:
                 if e.response['Error']['Code'] == "304":
-                    logging.debug("ETag matches md5 of local copy, using local copy of DB!")
+                    logging_debug(
+                        "ETag matches md5 of local copy, using local copy of DB!",
+                    )
                     self.db_hash = etag
                 else:
-                    logging.debug("Couldn't load remote DB object.")
+                    logging_debug("Couldn't load remote DB object.")
             except Exception as e:
                 # Weird one
-                logging.debug(e)
+                logging_debug(e)
 
         # SQLite DatabaseWrapper will treat our tmp as normal now
         # Check because Django likes to call this function a lot more than it should
@@ -67,10 +78,10 @@ class DatabaseWrapper(DatabaseWrapper):
             self.settings_dict['NAME'] = '/tmp/' + self.settings_dict['NAME']
 
         # Make sure it exists if it doesn't yet
-        if not os.path.isfile(self.settings_dict['NAME']):
+        if not path.isfile(self.settings_dict['NAME']):
             open(self.settings_dict['NAME'], 'a').close()
 
-        logging.debug("Loaded remote DB!")
+        logging_debug("Loaded remote DB!")
 
     def __init__(self, *args, **kwargs):
         super(DatabaseWrapper, self).__init__(*args, **kwargs)
@@ -93,19 +104,22 @@ class DatabaseWrapper(DatabaseWrapper):
             with open(self.settings_dict['NAME'], 'rb') as f:
                 fb = f.read()
 
-                m = hashlib.md5()
+                m = md5()
                 m.update(fb)
                 if self.db_hash == m.hexdigest():
-                    logging.debug("Database unchanged, not saving to remote DB!")
+                    logging_debug("Database unchanged, not saving to remote DB!")
                     return
 
                 bytesIO = BytesIO()
                 bytesIO.write(fb)
                 bytesIO.seek(0)
 
-                s3_object = s3.Object(self.settings_dict['BUCKET'], self.settings_dict['REMOTE_NAME'])
-                result = s3_object.put('rb', Body=bytesIO)
+                s3_object = s3.Object(
+                    self.settings_dict['BUCKET'],
+                    self.settings_dict['REMOTE_NAME'],
+                )
+                s3_object.put('rb', Body=bytesIO)
         except Exception as e:
-            logging.debug(e)
+            logging_debug(e)
 
-        logging.debug("Saved to remote DB!")
+        logging_debug("Saved to remote DB!")
